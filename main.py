2d74d864e579677bc699cd805d72171d4f04a7c7
Lilian Matsumoto <109561962+liliansom@users.noreply.github.com>
2023-02-11 02:13:26 -0300

    Arrumada a formatação do código
    Retirado espaços extras.

--- main.py
+++ main.py
@@ -13,49 +13,28 @@ cursor = connection.cursor()
 
 def adicionar():
     if len(EntNome.get()) < 1 or len(EntIdade.get()) < 0 or len(EntJog.get()) < 0 or len(EntEdit.get()) < 0:
-        messagebox.showerror(
-            "Erro 2",
-            'Preencha todos os campos')
+        messagebox.showerror("Erro 2", 'Preencha todos os campos')
         return
 
     cursor.execute(f'''
     INSERT INTO 
-        Catalogo (
-        Nome,
-        Idade,
-        Jogadores,
-        Editora,
-        Obs)
+        Catalogo (Nome, Idade, Jogadores, Editora, Obs)
     VALUES(
         '{EntNome.get().strip().upper()}',
         '{EntIdade.get().strip().upper()}',
         '{EntJog.get().strip().upper()}',
         '{EntEdit.get().strip().upper()}',
-        '{EntDesc.get(
-            '1.0',
-            'end')}')''')
+        '{EntDesc.get('1.0','end')}')''')
     cursor.commit()
 
 
 def limpar():
-    EntId.delete(
-        '0',
-        'end')
-    EntNome.delete(
-        '0',
-        'end')
-    EntIdade.delete(
-        '0',
-        'end')
-    EntJog.delete(
-        '0',
-        'end')
-    EntEdit.delete(
-        '0',
-        'end')
-    EntDesc.delete(
-        '1.0',
-        'end')
+    EntId.delete('0','end')
+    EntNome.delete('0','end')
+    EntIdade.delete('0','end')
+    EntJog.delete('0','end')
+    EntEdit.delete('0','end')
+    EntDesc.delete('1.0','end')
 
 
 def deletar():
@@ -75,19 +54,12 @@ def deletar():
 
 def procurar():
     if len(EntId.get()) < 0:
-        messagebox.showerror(
-            "Erro 1",
-            'Adicione o Id do jogo...')
+        messagebox.showerror("Erro 1", 'Adicione o Id do jogo...')
         return
 
     cursor.execute(f'''
     SELECT 
-        Id,
-        Nome,
-        Idade,
-        Jogadores,
-        Editora,
-        Obs        
+        Id, Nome, Idade, Jogadores, Editora, Obs        
     FROM 
         Catalogo 
     WHERE
@@ -95,42 +67,18 @@ def procurar():
         Nome='{EntNome.get().strip().upper()}' 
     ''')
     busca = cursor.fetchone()
-    EntId.delete(
-        '0',
-        'end')
-    EntNome.delete(
-        '0',
-        'end')
-    EntIdade.delete(
-        '0',
-        'end')
-    EntJog.delete(
-        '0',
-        'end')
-    EntEdit.delete(
-        '0',
-        'end')
-    EntDesc.delete(
-        '1.0',
-        'end')
-    EntId.insert(
-        0,
-        '{}'.format(busca[0]))
-    EntNome.insert(
-        0,
-        '{}'.format(busca[1]))
-    EntIdade.insert(
-        0,
-        '{} ANOS'.format(busca[2]))
-    EntJog.insert(
-        0,
-        '{} JOGADORES'.format(busca[3]))
-    EntEdit.insert(
-            0,
-            '{}'.format(busca[4]))
-    EntDesc.insert(
-        '1.0',
-        '{}'.format(busca[5]))
+    EntId.delete('0', 'end')
+    EntNome.delete('0', 'end')
+    EntIdade.delete('0','end')
+    EntJog.delete('0', 'end')
+    EntEdit.delete('0', 'end')
+    EntDesc.delete('1.0', 'end')
+    EntId.insert(0, '{}'.format(busca[0]))
+    EntNome.insert(0,'{}'.format(busca[1]))
+    EntIdade.insert(0, '{} ANOS'.format(busca[2]))
+    EntJog.insert(0, '{} JOGADORES'.format(busca[3]))
+    EntEdit.insert(0, '{}'.format(busca[4]))
+    EntDesc.insert('1.0', '{}'.format(busca[5]))
 
 
 def jan2():
@@ -154,65 +102,31 @@ def jan2():
         True,
         True)
     Bg3 = PhotoImage(file='CatTT/Bg3.png')
-    canvas1.create_image(
-        350,
-        250,
-        image=Bg3)
+    canvas1.create_image(350, 250, image=Bg3)
 
     BtFech = PhotoImage(file='CatTT/BtFech.png')
     btfech = tk.Button(
         janela2,
         image=BtFech,
         command=janela2.destroy)
-    btfech.place(
-        x=590,
-        y=430)
+    btfech.place(x=590, y=430)
     # TREE
     tree = tkinter.ttk.Treeview(
         janela2,
-        columns=[
-            '0',
-            '1',
-            '2',
-            '3',
-            '4'],
+        columns=['0', '1', '2', '3', '4'],
         show='headings')
 
-    tree.column(
-        '0',
-        width=5)
-    tree.column(
-        '1',
-        width=120)
-    tree.column(
-        '2',
-        width=80)
-    tree.column(
-        '3',
-        width=80)
-    tree.column(
-        '4',
-        width=100)
-    tree.heading(
-        '0',
-        text='Id')
-    tree.heading(
-        '1',
-        text='Nome')
-    tree.heading(
-        '2',
-        text='Classificação Etária')
-    tree.heading(
-        '3',
-        text='Número de Jogadores')
-    tree.heading(
-        '4',
-        text='Editora')
-    tree.place(
-        x=135,
-        y=80,
-        width=600,
-        height=325)
+    tree.column('0', width=5)
+    tree.column('1', width=120)
+    tree.column('2', width=80)
+    tree.column('3', width=80)
+    tree.column('4', width=100)
+    tree.heading('0', text='Id')
+    tree.heading('1', text='Nome')
+    tree.heading('2', text='Classificação Etária')
+    tree.heading('3', text='Número de Jogadores')
+    tree.heading('4', text='Editora')
+    tree.place(x=135, y=80, width=600, height=325)
 
     busca2 = cursor.execute('''
     SELECT 
@@ -224,11 +138,7 @@ def jan2():
     a = cursor.fetchall()
     cursor.commit()
     for row in a:
-        tree.insert("", tk.END, values=(row[0],
-                                        row[1],
-                                        row[2],
-                                        row[3],
-                                        row[4]))
+        tree.insert("", tk.END, values=(row[0], row[1], row[2], row[3], row[4]))
 
 
 # Interface gráfica
@@ -245,102 +155,51 @@ canvas = Canvas(
     bd=0,
     highlightthickness=0,
     relief="ridge")
-canvas.place(
-    x=0,
-    y=0)
-janela.resizable(
-    False,
-    False)
+canvas.place(x=0, y=0)
+janela.resizable(False, False)
 
 Bg2 = PhotoImage(file='CATALOGO/Bg2.png')
-canvas.create_image(
-    350,
-    250,
-    image=Bg2)
+canvas.create_image(350, 250, image=Bg2)
 
 # Entradas
 EntId = tk.Entry()
-EntId.place(
-    x=160,
-    y=70,
-    width=50,
-    height=30)
+EntId.place(x=160, y=70, width=50, height=30)
 
 EntNome = tk.Entry()
-EntNome.place(
-    x=280,
-    y=70,
-    width=430,
-    height=30)
+EntNome.place(x=280, y=70, width=430, height=30)
 
 EntIdade = tk.Entry()
-EntIdade.place(
-    x=160,
-    y=110,
-    width=550,
-    height=30)
+EntIdade.place(x=160, y=110, width=550, height=30)
 
 EntJog = tk.Entry()
-EntJog.place(
-    x=160,
-    y=150,
-    width=550,
-    height=30)
+EntJog.place(x=160, y=150, width=550, height=30)
 
 EntEdit = tk.Entry()
-EntEdit.place(
-    x=160,
-    y=190,
-    width=550,
-    height=30)
+EntEdit.place(x=160, y=190, width=550, height=30)
 
 EntDesc = tk.Text()
-EntDesc.place(
-    x=160,
-    y=230,
-    width=550,
-    height=170)
+EntDesc.place(x=160, y=230, width=550, height=170)
 
 # Botões
 BtAdicionar = PhotoImage(file='CATALOGO/BtAdicionar.png')
-btAdicionar = tk.Button(
-    image=BtAdicionar,
-    command=adicionar)
-btAdicionar.place(
-    x=160,
-    y=430)
+btAdicionar = tk.Button(image=BtAdicionar, command=adicionar)
+btAdicionar.place(x=160, y=430)
 
 BtProcurar = PhotoImage(file='CATALOGO/BtProcurar.png')
-btProcurar = tk.Button(
-    image=BtProcurar,
-    command=procurar)
-btProcurar.place(
-    x=310,
-    y=430)
+btProcurar = tk.Button(image=BtProcurar, command=procurar)
+btProcurar.place(x=310, y=430)
 
 BtDeletar = PhotoImage(file='CATALOGO/BtDeletar.png')
-btDeletar = tk.Button(
-    image=BtDeletar,
-    command=deletar)
-btDeletar.place(
-    x=460,
-    y=430)
+btDeletar = tk.Button(image=BtDeletar, command=deletar)
+btDeletar.place(x=460, y=430)
 
 BtLimpar = PhotoImage(file='CATALOGO/BtLimpar.png')
-btLimpar = tk.Button(
-    image=BtLimpar,
-    command=limpar)
-btLimpar.place(
-    x=610,
-    y=430)
+btLimpar = tk.Button(image=BtLimpar, command=limpar)
+btLimpar.place(x=610, y=430)
 
 BtCol = PhotoImage(file='CATALOGO/BtCat.png')
-btCol = tk.Button(
-    image=BtCol,
-    command=jan2)
-btCol.place(
-    x=20,
-    y=398)
+btCol = tk.Button(image=BtCol, command=jan2)
+btCol.place(x=20, y=398)
 
 janela.mainloop()
 
